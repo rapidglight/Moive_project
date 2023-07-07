4470a1567fc861ed11030dc589f9d663f95b1a6e
Phoenix-0921 <jyu85921@gmail.com>
2023-07-07 11:48:05 +0800

    新增moviedataRespones

---
+++ src/main/java/com/example/movie_project/model/MoviedataRespones.java
@@ -0,0 +1,13 @@
+package com.example.movie_project.model;
+
+import lombok.Data;
+
+@Data
+public class MoviedataRespones extends BaseResponse {
+
+    public MoviedataRespones(int code, String message) {
+        super(code, message);
+        // TODO Auto-generated constructor stub
+    }
+
+}
