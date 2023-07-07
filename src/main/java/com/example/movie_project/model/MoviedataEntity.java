4371cb2bd9b5002a7af98c2e0f886ad60d662fcb
Phoenix-0921 <jyu85921@gmail.com>
2023-07-07 11:33:29 +0800

    新增dataentity

---
+++ src/main/java/com/example/movie_project/model/MoviedataEntity.java
@@ -0,0 +1,19 @@
+//電影頁面資料entity
+
+package com.example.movie_project.model;
+
+public class MoviedataEntity {
+    int movieId;
+    String movieName;
+    String imageUrl;
+    int year;
+    String age;
+    int movieLong;
+    int movieCategoryRomance;
+    int movieCategoryComedy;
+    int movieCategoryDrama;
+    int movieCategoryAction;
+    String movieDescription;
+    String movieDirector;
+    String movieCast;
+}
