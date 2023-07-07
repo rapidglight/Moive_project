36fca2689e0133be3f4e5968d2b308e19f5a363b
Phoenix-0921 <jyu85921@gmail.com>
2023-07-07 16:46:42 +0800

    新增ＳＱＬ黨

---
+++ src/main/sql/userdata_202307071644.sql
@@ -0,0 +1,9 @@
+INSERT INTO javaconnect.userdata (user_account,user_password,user_mail) VALUES
+	 ('1234','1234',NULL),
+	 ('2345','asd8743',NULL),
+	 ('32345t623','asd8743',NULL),
+	 ('4321','asd8743',NULL),
+	 ('432145','asd8743',NULL),
+	 ('432556','asd8743',NULL),
+	 ('clark84322','ricky8743',NULL),
+	 ('qwe84322','asd8743',NULL);
