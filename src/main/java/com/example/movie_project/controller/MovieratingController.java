609d8a67fac3363fdbc17d8d6d12e1398daa2b95
Phoenix-0921 <jyu85921@gmail.com>
2023-07-10 17:41:03 +0800

    更新java

--- src/main/java/com/example/movie_project/controller/MovieratingController.java
+++ src/main/java/com/example/movie_project/controller/MovieratingController.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
 
 import org.springframework.http.MediaType;
@@ -25,35 +26,7 @@ public class MovieratingController {
         return getMovieratingList();
     }
 
-    @RequestMapping(value = "/movierating", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, // 傳入的資料格式
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public BaseResponse addmovierating(@RequestBody MovieratingEntity data) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/projectdata?user=root&password=0000");
-
-            stmt = conn.prepareStatement("INSERT INTO storesystem VALUES(null, ?, ?, ?, ?, )");
-            stmt.setString(1, data.getUserAccount());
-            stmt.setString(2, data.getUserRatingMovieComments());
-            stmt.setString(3, data.getUserRatingMovieName());
-            stmt.setInt(4, data.getUserRatingMovieStars());
-            stmt.setInt(5, data.getRatinglistId());
-
-            stmt.executeUpdate();
-
-            return new BaseResponse(0, "新增成功");
-
-        } catch (SQLException e) {
-            return new BaseResponse(e.getErrorCode(), e.getMessage());
-        } catch (ClassNotFoundException e) {
-            return new BaseResponse(1, "無法註冊驅動程式");
-        }
-    }
-
-    @RequestMapping(value = "/movierating", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, // 傳入的資料格式
+    @RequestMapping(value = "/movieratings", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, // 傳入的資料格式
             produces = MediaType.APPLICATION_JSON_VALUE)
     public BaseResponse updateMovierating(@RequestBody MovieratingEntity data) {
         Connection conn = null;
@@ -61,17 +34,21 @@ public class MovieratingController {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/javaconnent?user=root&password=0000");
-
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/javaconnect?user=root&password=0000");
             stmt = conn.prepareStatement(
-                    "UPDATE user_rating_list SET  user_rating_moviestars=? WHERE id=?");
-            stmt.setInt(4, data.getUserRatingMovieStars());
-            stmt.setInt(5, data.getRatinglistId());
+                    "INSERT INTO user_rating_list (user_account, user_rating_movie_stars, user_rating_movie_name) " +
+                            "VALUES (?, ?, ?) " +
+                            "ON DUPLICATE KEY UPDATE " +
+                            "user_rating_movie_stars = VALUES(user_rating_movie_stars)");
 
-            stmt.executeUpdate();
+            stmt.setNull(1, Types.INTEGER); // 自動生成 rating_list_id
+            stmt.setString(2, data.getUserAccount()); // 使用者帳號
+            stmt.setInt(3, data.getUserRatingMovieStars()); // 使用者評分值
+            stmt.setString(4, data.getUserRatingMovieName()); // 電影名稱
+
+            int rowsAffected = stmt.executeUpdate();
 
             return new BaseResponse(0, "更新成功");
-
         } catch (SQLException e) {
             return new BaseResponse(e.getErrorCode(), e.getMessage());
         } catch (ClassNotFoundException e) {
@@ -86,18 +63,18 @@ public class MovieratingController {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/projectdata?user=root&password=0000");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/javaconnect?user=root&password=0000");
             stmt = conn.createStatement();
             rs = stmt.executeQuery("select * from user_rating_list");// 這裡後續要修改資料庫路徑以及要修改的項目
 
             ArrayList<MovieratingEntity> movies = new ArrayList<>();
             while (rs.next()) {
                 MovieratingEntity MovieratingEntity = new MovieratingEntity();
-                MovieratingEntity.setRatinglistId(rs.getInt("id"));
+                MovieratingEntity.setRatinglistId(rs.getInt("rating_list_id"));
                 MovieratingEntity.setUserAccount(rs.getString("user_account"));
                 MovieratingEntity.setUserRatingMovieComments(rs.getString("user_rating_movie_comments"));
                 MovieratingEntity.setUserRatingMovieName(rs.getString("user_rating_movie_name"));
-                MovieratingEntity.setUserRatingMovieStars(rs.getInt("user_rating_movie_name"));
+                MovieratingEntity.setUserRatingMovieStars(rs.getInt("user_rating_movie_stars"));
                 movies.add(MovieratingEntity);
             }
             return new MovieratingResponse(0, "成功", movies);
