4ca3884c42529d9373f81c7a48f355a74acbec4d
rapidglight <138113259+rapidglight@users.noreply.github.com>
2023-07-11 17:27:17 +0800

    主頁 網站排行榜完成

---
+++ src/main/java/com/example/movie_project/controller/WebsiteRatingController.java
@@ -0,0 +1,67 @@
+package com.example.movie_project.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.movie_project.model.WebsiteRatingEntity;
+import com.example.movie_project.model.WebsiteRatingResponse;
+
+@RestController
+
+public class WebsiteRatingController {
+    @RequestMapping(value = "/websiterating", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebsiteRatingResponse movies() {
+        return getWebsiteRating();
+
+    }
+
+    private WebsiteRatingResponse getWebsiteRating() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/javaconnect?user=root&password=0000");
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(
+                    "select movie.movie_id,movie.image_url,movie.movie_name,user_rating_movie_name,avg(user_rating_movie_stars) \r\n"
+                            + //
+                            "from user_rating_list join javaconnect.movie  where user_rating_movie_name=movie_name \r\n"
+                            + //
+                            "group by user_rating_movie_name order by user_rating_movie_stars desc limit 5");
+
+            ArrayList<WebsiteRatingEntity> movies = new ArrayList<>();
+
+            while (rs.next()) {
+                WebsiteRatingEntity websiteRatingEntity = new WebsiteRatingEntity();
+                websiteRatingEntity.setMovieId(rs.getInt("movie_id"));
+                websiteRatingEntity.setImageUrl(rs.getString("image_url"));
+                websiteRatingEntity.setUserRatingMovieName(rs.getString("user_rating_movie_name"));
+                websiteRatingEntity.setUserRatingMovieStars(rs.getInt("avg(user_rating_movie_stars)"));
+
+                movies.add(websiteRatingEntity);
+
+            }
+            return new WebsiteRatingResponse(0, "成功", movies);
+
+        } catch (SQLException e) {
+            return new WebsiteRatingResponse(e.getErrorCode(), e.getMessage(), null);
+        } catch (ClassNotFoundException e) {
+            return new WebsiteRatingResponse(1, "無法註冊驅動程式", null);
+        }
+
+    }
+
+}
---
+++ src/main/java/com/example/movie_project/model/WebsiteRatingEntity.java
@@ -0,0 +1,11 @@
+package com.example.movie_project.model;
+
+import lombok.Data;
+
+@Data
+public class WebsiteRatingEntity {
+    int MovieId;
+    String imageUrl;
+    String userRatingMovieName;
+    int userRatingMovieStars;
+}
