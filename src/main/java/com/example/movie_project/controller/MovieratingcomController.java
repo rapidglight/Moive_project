64267ebea91025287eab34664bc53f4810a5fdc0
Phoenix-0921 <jyu85921@gmail.com>
2023-07-13 01:25:27 +0800

    讀取評論

---
+++ src/main/java/com/example/movie_project/controller/MovieratingcomController.java
@@ -0,0 +1,54 @@
+package com.example.movie_project.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.movie_project.model.BaseResponse;
+import com.example.movie_project.model.MovieratingEntity;
+import com.example.movie_project.model.MovieratingResponse;
+
+@RestController
+public class MovieratingcomController {
+
+    @RequestMapping(value = "/movieratingcom", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, // 傳入的資料格式
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public BaseResponse updateMovierating(@RequestBody MovieratingEntity data) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/javaconnect?user=root&password=0000");
+            stmt = conn.prepareStatement(
+                    "INSERT INTO user_rating_list (user_account, user_rating_movie_comments, user_rating_movie_name) " +
+                            "VALUES (?, ?, ?) " +
+                            "ON DUPLICATE KEY UPDATE " +
+                            "user_rating_movie_comments = VALUES(user_rating_movie_comments)");
+
+            stmt.setString(1, data.getUserAccount());
+            stmt.setString(2, data.getUserRatingMovieComments());
+            stmt.setString(3, data.getUserRatingMovieName());
+
+            int rowsAffected = stmt.executeUpdate();
+
+            return new BaseResponse(0, "更新成功");
+        } catch (SQLException e) {
+            return new BaseResponse(e.getErrorCode(), e.getMessage());
+        } catch (ClassNotFoundException e) {
+            return new BaseResponse(1, "無法註冊驅動程式");
+        }
+    }
+
+}
