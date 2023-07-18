5db46a5952e598ad4e45b850650251e701fd3005
Phoenix-0921 <jyu85921@gmail.com>
2023-07-18 16:00:05 +0800

    加個聊天室看看

---
+++ src/main/java/com/example/movie_project/model/ChatClientModel.java
@@ -0,0 +1,14 @@
+package com.example.movie_project.model;
+
+public class ChatClientModel {
+    private String Clientname;
+
+    public String getClientname() {
+        return Clientname;
+    }
+
+    public void setClientname(String clientname) {
+        Clientname = clientname;
+    }
+
+}
---
+++ src/main/java/com/example/movie_project/model/ServerResponseModel.java
@@ -0,0 +1,14 @@
+package com.example.movie_project.model;
+
+public class ServerResponseModel {
+    private String responseMessage;
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+}
